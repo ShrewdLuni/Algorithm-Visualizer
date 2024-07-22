8f81e84835efe97278f5d4c378e924539be94538
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-22 23:29:17 +0200

    feat: add not found page

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,16 @@ import App from './App.tsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { SortingPage } from './pages/Sort/SortingPage.tsx';
+import { NotFoundPage } from './pages/NotFound/NotFound.tsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <SortingPage/>,
   },
   {
-    path: "/sort",
-    element: <SortingPage/>,
+    path: "/error",
+    element: <NotFoundPage/>,
   },
 ]);
 
---
+++ src/pages/NotFound/NotFound.tsx
@@ -0,0 +1,14 @@
+export const NotFoundPage = () => {
+  return (
+    <div className="flex flex-col h-screen w-screen bg-black justify-center items-center text-left">
+      <div className="flex flex-col w-[90%]">
+        <p className="block w-[100%] text-white text-[12vw] md:text-[6vw] font-bold">404</p> 
+        <div className="flex flex-col">
+          <p className="text-white text-[8vw] md:text-[4vw] font-semibold">Something went wrong.</p>
+          <p className="text-gray-400 text-[4vw] md:text-[2vw]">The page is missing or you assembled the link incorrectly</p>
+        </div>
+        <a className="text-blue-500 text-[4vw] md:text-[2vw] mt-8" href="/">Go back to home page</a>
+      </div>
+    </div>
+  )
+}
