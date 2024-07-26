6e6cba92a9f569693926dc648183b0429b8b1e08
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-07-26 22:51:59 +0300

    change style

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -5,9 +5,9 @@ export const NotFoundPage = () => {
         <p className="block w-[100%] text-white text-[12vw] md:text-[6vw] font-bold">404</p> 
         <div className="flex flex-col">
           <p className="text-white text-[8vw] md:text-[4vw] font-semibold">Something went wrong.</p>
-          <p className="text-gray-400 text-[4vw] md:text-[2vw]">The page is missing or you assembled the link incorrectly</p>
+          <p className="text-gray-400 text-[5vw] md:text-[2vw]">The page is missing or you assembled the link incorrectly</p>
         </div>
-        <a className="text-blue-500 text-[4vw] md:text-[2vw] mt-8" href="/">Go back to home page</a>
+        <a className="text-blue-500 text-[5vw] md:text-[2vw] mt-8" href="/">Go back to home page</a>
       </div>
     </div>
   )
