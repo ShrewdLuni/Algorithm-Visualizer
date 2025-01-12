642110627a974e9d7c303061c7288b4736cbd8ad
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2025-01-12 21:16:55 +0100

    polishing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { SortingPage } from './pages/Sort/SortingPage.tsx';
-import { NotFoundPage } from './pages/NotFound/NotFound.tsx';
+import { SortingPage } from './pages/SortingPage.tsx';
+import { NotFoundPage } from './pages/NotFound.tsx';
 
 const router = createBrowserRouter([
   {
